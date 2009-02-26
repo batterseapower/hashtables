f19b900e7d3b510413b82ba7ba39b5e7211c8699
Max Bolingbroke <batterseapower@hotmail.com>
2009-02-26 21:07:20 +0000

    Attempt to test some monadic properties on HashTables and fail

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -12,6 +12,12 @@ import Data.Array.ST ( STArray )
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2
 
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+
+
+import Prelude hiding ( lookup )
+
 
 tests :: [Test]
 tests = [
@@ -27,5 +33,9 @@ data_hashtable_marray_tests = $(testProperties [d|
             ht <- new :: ST s (HashTable (STArray s) (ST s) Int Int)
             fmap null (toList ht)
         
-        --prop_insert_changes_value_at_key ht = runST $ ht
+        prop_insert_changes_value_at_key ht key val = monadic runST $ do
+            value_afterwards <- run $ do
+                insert (ht :: HashTable (STArray s) (ST s) Int Int) key val
+                lookup ht key
+            assert (value_afterwards == Just val)
     |])
